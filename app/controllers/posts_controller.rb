2b5295df9e8e4965137f129f33c00ba499f6544c
VIJAY TADIMETI <vijayvardhan.94@gmail.com>
2018-09-22 18:14:12 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all.order('created_at DESC')
+	end
+
+	def new 
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :body)
+	end
+end
